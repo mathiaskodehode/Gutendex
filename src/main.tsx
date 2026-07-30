3529644ce971f872323afce8b7d87dc2470e8cde
Mathias <4mathias.kodehode@gmail.com>
2026-07-30 08:47:44 +0200

    add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { StrictMode } from "react";
+import { RouterProvider } from "react-router/dom";
+import { router } from "./router/index.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const root = document.getElementById("root")!;
+createRoot(root).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
+);
---
+++ src/router/index.tsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [{ index: true, element: <Home /> }],
+    },
+    { path: "*", element: <p>404 - PAGE NOT FOUND</p> },
+]);
