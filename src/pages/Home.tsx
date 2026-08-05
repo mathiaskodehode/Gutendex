f855821f1bc0d9bd42074394460a78a9055b8fd4
Mathias <4mathias.kodehode@gmail.com>
2026-08-05 09:01:06 +0200

    minor formatting

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,15 +3,13 @@ import { useBooks } from "../context/BooksContext";
 export default function Home() {
     const { data, loading, error } = useBooks();
 
-    // TODO: add spinner
     if (loading) {
+        // TODO: add spinner
         return <p>Loading books...</p>;
     }
-
     if (error) {
         return <p>Error: {error}</p>;
     }
-
     if (!data) {
         return <p>Select a category</p>;
     }
