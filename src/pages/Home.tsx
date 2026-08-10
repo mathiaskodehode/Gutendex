c694cdd5332584b553b0a9c93d7ac118f6b13160
Mathias <4mathias.kodehode@gmail.com>
2026-08-10 14:38:57 +0200

    add TODOs to home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,5 @@
+// TODO: add search bar like google.com has.
+// TODO: add quick actions(?) menu for common actions
 export default function Home() {
-    return (
-        <>
-            {/* <Favorites /> */}
-            {/* <BookList /> */}
-            {/* <Pagination /> */}
-        </>
-    );
+    return <></>;
 }
